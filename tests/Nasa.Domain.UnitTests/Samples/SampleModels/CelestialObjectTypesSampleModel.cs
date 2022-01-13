102b91542dfb64fa95af3d3cdf0a3ee752820c38
nuyonu <nuyonu@gmail.com>
2022-01-13 23:10:15 +0200

    Add domain tests

---
+++ tests/Nasa.Domain.UnitTests/Samples/SampleModels/CelestialObjectTypesSampleModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Nasa.Domain.UnitTests.Samples.SampleModels;
+
+public class CelestialObjectTypesSampleModel
+{
+    public string Name { get; set; }
+
+    public double Mass { get; set; }
+
+    public double EquatorialDiameter { get; set; }
+
+    public double SurfaceTemperature { get; set; }
+
+    public DateTime DiscoveryDate { get; set; }
+
+    public Guid DiscoverySourceId { get; set; }
+
+    public string ExpectedType { get; set; }
+}
